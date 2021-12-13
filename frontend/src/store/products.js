218e560c66207c58b0eca1ba168e94edfdeac017
your name here <mmakhambetov@yandex.ru>
2021-12-13 23:17:22 +0400

    Create a product

--- frontend/src/store/products.js
+++ frontend/src/store/products.js
@@ -1,4 +1,5 @@
 const ADD_PRODUCTS = 'products/ADD_PRODUCTS';
+const ADD_ONE_PRODUCT = 'products/ADD_ONE_PRODUCT';
 
 const addProducts = (payload) => {
   return {
@@ -7,19 +8,44 @@ const addProducts = (payload) => {
   }
 };
 
+const addOneProduct = (payload) => {
+  return {
+    type: ADD_ONE_PRODUCT,
+    payload
+  }
+}
+
 export const getAllProducts = () => async (dispatch) => {
   const res = await fetch('/api/products');
   const data = await res.json();
   if (res.ok) dispatch(addProducts(data.products));
 };
 
+export const addProduct = (product) => async (dispatch) => {
+  const res = await fetch('/api/products', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(product),
+  })
+  
+  if (res.ok) {
+    const data = await res.json();
+    console.log('data', data);
+    dispatch(addOneProduct(data.product));
+  }
+};
+
 const productReducer = (state = {}, action) => {
-  const newState = {};
+  let newState = {};
   switch (action.type) {
     case ADD_PRODUCTS:
       action.payload.forEach(product => {
         newState[product.id] = product;
       });
+      return newState;
+    case ADD_ONE_PRODUCT:
+      newState = { ...state };
+      newState[action.payload.id] = action.payload;
       return newState;
     default:
       return state;
